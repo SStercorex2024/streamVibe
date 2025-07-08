062af511f8e28eedcdca9899d18900f9c9cfe61a
stercorexze <stercorexze@gmail.com>
2025-07-08 21:44:53 +0200

    added footer.

---
+++ src/components/Socials/Socials.jsx
@@ -0,0 +1,34 @@
+import './Socials.scss'
+import classNames from "classnames";
+import Button from "@/components/Button";
+
+const Socials = (props) => {
+  const {
+    className,
+    links = [],
+  } = props
+
+  return (
+    <div className={classNames(className, 'soc1al')}
+    >
+      <ul className="soc1als__list">
+        {links.map(({label, iconName}, index) => (
+          <li className="soc1als__item" key={index}>
+            <Button
+              className="soc1als__link"
+              mode="black-10"
+              href="/"
+              traget="_blank"
+              labrl={label}
+              isLabelHidden
+              hasFillIcon
+              iconName={iconName}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Socials;
--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,5 +1,98 @@
+import './Footer.scss'
+import Socials from "@/components/Socials";
+
 const Footer = () => {
-  return <footer>FOOTER</footer>
+  const menuItems = [
+    {
+      title: 'Home',
+      mainHref: '/home',
+      link: ['Categories', 'Devices', 'Pricing', 'FAQ'],
+      href: ['/categories', '/devices', '/pricing', '/faq'],
+    },
+    {
+      title: 'Movies',
+      mainHref: '/movies',
+      link: ['Gernes', 'Trending', 'New Release', 'Popular'],
+      href: ['/gernes', '/trending', '/new-release', '/popular'],
+    },
+    {
+      title: 'Shows',
+      mainHref: '/shows',
+      link: ['Gernes', 'Trending', 'New Release', 'Popular'],
+      href: ['/gernes', '/trending', '/new-release', '/popular'],
+    },
+    {
+      title: 'Support',
+      mainHref: '/support',
+      link: ['Contact Us'],
+      href: ['/contact-us'],
+    },
+    {
+      title: 'Subscription',
+      mainHref: '/subscription',
+      link: ['Plans', 'Features'],
+      href: ['/plans', '/features'],
+    },
+    {
+      title: 'Connect With Us',
+      mainHref: '/connect-with-us',
+      socialLinks: [
+        {
+          label: 'Facebook',
+          iconName: 'facebook',
+        },
+        {
+          label: 'Twitter',
+          iconName: 'twitter',
+        },
+        {
+          label: 'LinkedIn',
+          iconName: 'linked-in',
+        },
+
+      ]
+    },
+  ]
+
+  const extraLinks = ['Terms of Use', 'Privacy Policy', 'Cookie Policy']
+  const extraHrefs = ['/terms-of-use', '/privacy-policy', '/cookie-policy']
+
+  return <footer>
+    <div className="footer__inner container">
+      <nav className="footer__menu">
+        {menuItems.map(({title, mainHref, link, socialLinks, href}, index,) => (
+          <div className="footer__menu-column" key={index}>
+            <a className="footer__menu-title h6" href={mainHref}>{title}</a>
+            {link?.length > 0 && (
+              <ul className="footer__menu-list">
+                {link.map((item, index) => (
+                  <li className="footer__menu-item" key={index}>
+                    <a className="footer__menu-link" href={href?.[0]}>{item}</a>
+                  </li>
+                ))}
+              </ul>
+            )}
+            {socialLinks?.length > 0 && (
+              <Socials
+                className="footer__soc1als"
+                links={socialLinks}
+              />
+            )}
+          </div>
+        ))}
+      </nav>
+      <div className="footer__extra">
+        <p className="footer__copyright">
+          @ <time dateTime="2023">2023</time> streamvib, All Rights Reserved
+        </p>
+        <div className="footer__extra-links">
+          {extraLinks.map((text, index) => (
+            <a className="footer__extra-link" href={extraHrefs[index]} key={index}>{text}</a>
+          ))}
+        </div>
+      </div>
+    </div>
+  </footer>
 }
 
 export default Footer

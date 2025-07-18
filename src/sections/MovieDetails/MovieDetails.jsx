4cd028c5fbe60b4d7c40e7291ecd52165aa861bb
stercorexze <stercorexze@gmail.com>
2025-07-18 11:50:23 +0200

    added component personCard, started section movieDetails.

---
+++ src/sections/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,66 @@
+import './MovieDetails.scss'
+import SliderNavigation from "@/components/Slider/components/SliderNavigation";
+import Slider from "@/components/Slider";
+
+const MovieDetails = () => {
+  const titleId = 'movie-details-title'
+  const customSliderNavigationId = 'movie-card-slider-navigation'
+
+  return (
+    <section
+      className="movie-details container"
+      aria-labelledby={titleId}
+    >
+      <h2 className="visually-hidden" id={titleId}>
+        Detailed movie information
+      </h2>
+      <div className="movie-details__main">
+        <div className="movie-details__panel">
+          <h3 className="movie-details__title">
+            Description
+          </h3>
+          <div className="movie-details__description">
+            <p>A fiery young man clashes with an unflinching forest officer in a
+              south Indian village where spirituality, fate and folklore rule
+              the lands.
+            </p>
+          </div>
+        </div>
+        <div className="movie-details__panel">
+          <header className="movie-details__panel-header">
+            <h3 className="movie-details__title">
+              Cast
+            </h3>
+            <SliderNavigation
+              id={customSliderNavigationId}
+              hasPagination={false}
+              mode='rounded'
+              buttonMode='black-08'
+            />
+          </header>
+          <Slider
+            navigationTargetElementId={customSliderNavigationId}
+            hasScrollbarOnMobile={false}
+            sliderParams={{
+              slidesPerView: 'auto',
+              spaceBetween: 10,
+              breakpoints: {
+                1024: {
+                  slidesPerView: 'auto',
+                  spaceBetween: 30,
+                  allowTouchMove: false,
+                }
+              }
+            }}
+          />
+        </div>
+        <div className="movie-details__panel"></div>
+      </div>
+      <aside className="movie-details__info">
+        <div className="movie-details__panel"></div>
+      </aside>
+    </section>
+  )
+}
+
+export default MovieDetails

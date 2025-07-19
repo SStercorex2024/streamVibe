91f81f1bac6b885e8c2fe90462e424d2af79b8cf
stercorexze <stercorexze@gmail.com>
2025-07-19 11:49:47 +0200

    started page show(one), added section showBanner.

--- src/sections/MovieBanner/MovieBanner.jsx
+++ src/sections/MovieBanner/MovieBanner.jsx
@@ -5,14 +5,16 @@ const MovieBanner = () => {
   const titleId = 'movie-banner-title'
 
   return (
-    <MovieBannerCard
-      title="Kantara"
-      titleId={titleId}
-      TitleTag="h1"
-      isSmallPaddingY
-      description="A fiery young man clashes with an unflinching forest officer in a south Indian village where spirituality, fate and folklore rule the lands."
-      imgSrc="/src/assets/images/movie-banner/1.jpg"
-    />
+    <section className='container' aria-labelledby={titleId}>
+      <MovieBannerCard
+        title="Kantara"
+        titleId={titleId}
+        TitleTag="h1"
+        isSmallPaddingY
+        description="A fiery young man clashes with an unflinching forest officer in a south Indian village where spirituality, fate and folklore rule the lands."
+        imgSrc="/src/assets/images/movie-banner/1.jpg"
+      />
+    </section>
   )
 }
 

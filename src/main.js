c34cb253097fd9bc1858a9bb0649f445cfb94c36
stercorexze <stercorexze@gmail.com>
2025-07-19 22:23:21 +0200

    added script for episodeCard.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import OverlayMenu from "@/modules/OvarlayMenu";
 import SliderCollection from "@/modules/SliderCollection";
 import TabsCollections from "@/modules/TabsCollections";
+import VideoPlayerCollection from "@/modules/VideoPlayerCollection";
 
 
 new OverlayMenu()
 new SliderCollection()
 new TabsCollections()
+new VideoPlayerCollection()
